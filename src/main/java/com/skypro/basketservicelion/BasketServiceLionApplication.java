ea40009a32e64398e0853ecad7dbc57f98919f2e
leush <leushkinml@mail.ru>
2022-11-30 11:07:43 +0300

    Initial commit

---
+++ src/main/java/com/skypro/basketservicelion/BasketServiceLionApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.basketservicelion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketServiceLionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketServiceLionApplication.class, args);
+    }
+
+}
